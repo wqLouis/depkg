4b97c9b8d249f9125b667de39a4577921dba4155
wqLouis <wongqiluo@gmail.com>
2026-01-22 01:40:45 +0800

    feat(tex parser): added mip map parsing

--- src/pkg_parser/tex_parser.rs
+++ src/pkg_parser/tex_parser.rs
@@ -1,6 +1,31 @@
-use std::io::{BufReader, Cursor, Read};
+use std::io::{BufReader, Cursor, Read, Seek};
 
-pub fn parse(bytes: &Vec<u8>) -> Vec<u8> {
+struct Signature {
+    // https://en.wikipedia.org/wiki/List_of_file_signatures
+    table: Vec<(Vec<u32>, &'static str)>, // (pattern, extension)
+}
+
+impl Signature {
+    pub fn new() -> Signature {
+        let mut sig = Signature { table: Vec::new() };
+        sig.table.push((vec![0xff, 0xd8, 0xff], "jpg"));
+        sig.table
+            .push((vec![0x89, 0x48, 0x44, 0x47, 0x0d, 0x0a, 0x1a, 0x0a], "png"));
+
+        sig
+    }
+
+    pub fn match_extension(bytes: &Vec<u8>) {}
+
+    fn vec_u8_to_u32(bytes: &[u8]) -> Vec<u32> {
+        bytes
+            .chunks_exact(4)
+            .map(|chunk| u32::from_le_bytes(chunk.try_into().unwrap()))
+            .collect()
+    }
+}
+
+pub fn parse(bytes: &Vec<u8>, parse_mipmap: bool) -> Vec<Vec<u8>> {
     const MAGIC: usize = 8;
     const SEP: i64 = 1;
     const TEX_SIZE: usize = 4;
@@ -12,6 +37,7 @@ pub fn parse(bytes: &Vec<u8>) -> Vec<u8> {
     let mut texb = [0u8; MAGIC];
     let mut size = [0u8; TEX_SIZE];
     let mut payload: Vec<u8>;
+    let mut payloads: Vec<Vec<u8>> = Vec::new();
 
     buf.read_exact(&mut texv).unwrap();
     buf.seek_relative(SEP).unwrap();
@@ -25,5 +51,30 @@ pub fn parse(bytes: &Vec<u8>) -> Vec<u8> {
 
     buf.read_exact(&mut payload).unwrap();
 
-    payload
+    payloads.push(payload.clone());
+
+    if !parse_mipmap {
+        return payloads;
+    }
+
+    let bytes_len = bytes.len();
+
+    // read mip map
+    loop {
+        let pos = buf.stream_position().unwrap();
+        if bytes_len == pos as usize {
+            break;
+        }
+        buf.seek_relative((MAGIC * 2) as i64).unwrap();
+        buf.read_exact(&mut size).unwrap();
+        let size = u32::from_le_bytes(size);
+        if size == 0 {
+            break;
+        }
+        payload = vec![0u8; size as usize];
+        buf.read_exact(&mut payload).unwrap();
+        payloads.push(payload.clone());
+    }
+
+    payloads
 }
