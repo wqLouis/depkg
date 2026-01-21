4789f50a9e4988c0d85670678a4f5336b62a929a
wqLouis <wongqiluo@gmail.com>
2026-01-21 21:12:26 +0800

    feat(tex parser): Parse tex as png.

--- src/pkg_parser/parser.rs
+++ src/pkg_parser/parser.rs
@@ -121,13 +121,13 @@ impl Pkg {
     pub fn save_pkg(&mut self, target: &Path) {
         for (path, bytes) in self.files.iter() {
             let mut path = target.join(path);
-            // create_dir_all(path.parent().unwrap()).unwrap();
+            create_dir_all(path.parent().unwrap()).unwrap();
             if path.extension().unwrap_or_default() == "tex" {
                 let parsed = tex_parser::parse(bytes);
-                path.set_extension(parsed.0);
-                // fs::write(path, parsed.1).unwrap();
+                path.set_extension("png"); // TODO: Support other formats
+                fs::write(path, parsed).unwrap();
             } else {
-                // fs::write(path, bytes).unwrap();
+                fs::write(path, bytes).unwrap();
             }
         }
     }
--- src/pkg_parser/tex_parser.rs
+++ src/pkg_parser/tex_parser.rs
@@ -1,5 +1,29 @@
-use std::io::{Cursor, Read, Seek, SeekFrom};
+use std::io::{BufReader, Cursor, Read};
 
-pub fn parse(bytes: &[u8]) -> (&str, Vec<u8>) {
-    let mut cursor = Cursor::new(bytes);
+pub fn parse(bytes: &Vec<u8>) -> Vec<u8> {
+    const MAGIC: usize = 8;
+    const SEP: i64 = 1;
+    const TEX_SIZE: usize = 4;
+
+    let mut buf = BufReader::new(Cursor::new(bytes));
+
+    let mut texv = [0u8; MAGIC]; // I have no idea what all this magic variables are
+    let mut texi = [0u8; MAGIC];
+    let mut texb = [0u8; MAGIC];
+    let mut size = [0u8; TEX_SIZE];
+    let mut payload: Vec<u8>;
+
+    buf.read_exact(&mut texv).unwrap();
+    buf.seek_relative(SEP).unwrap();
+    buf.read_exact(&mut texi).unwrap();
+    buf.seek_relative(SEP + (TEX_SIZE * 7) as i64).unwrap();
+    buf.read_exact(&mut texb).unwrap();
+    buf.seek_relative((MAGIC * 4) as i64 + SEP).unwrap();
+    buf.read_exact(&mut size).unwrap();
+
+    payload = vec![0u8; u32::from_le_bytes(size) as usize];
+
+    buf.read_exact(&mut payload).unwrap();
+
+    payload
 }
