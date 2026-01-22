94bc042d779612071776e67bd320404872351ec4
wqLouis <wongqiluo@gmail.com>
2026-01-23 00:50:10 +0800

    feat(main): command line args to pkg file

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,11 @@ use std::path::Path;
 use crate::pkg_parser::parser::Pkg;
 
 fn main() {
-    const PATH: &str = "./test/scene.pkg";
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 1 {
+        return;
+    }
 
-    let mut pkg = Pkg::new(Path::new(PATH));
+    let mut pkg = Pkg::new(Path::new(&args[0]));
     pkg.save_pkg(Path::new("./output"));
 }
